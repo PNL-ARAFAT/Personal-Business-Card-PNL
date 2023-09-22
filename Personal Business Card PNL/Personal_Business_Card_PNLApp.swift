29d49fffe70cd5a480a71eefa5f6ab7234b606d4
arafat2183 <akash076987@gmail.com>
2023-09-22 17:46:31 +0900

    Initial Commit

---
+++ Personal Business Card PNL/Personal_Business_Card_PNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Business_Card_PNLApp.swift
+//  Personal Business Card PNL
+//
+//  Created by Akash Arafat on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Business_Card_PNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
